cdff3d98c7e80dfb648e66075cfeb016c5e73a6d
estebanLopezUsma <esteban290604@gmail.com>
2025-11-02 23:36:47 -0500

    se agrego funciones nuevas animadas, como naves destruyendo meteoritos

--- gamificacionProyecto/views.py
+++ gamificacionProyecto/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index_view(request):
+    """
+    Esta vista se encarga de renderizar y mostrar la página principal.
+    """
     return render(request, 'index.html')
